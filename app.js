18ba4acb41465a55aa4af0a2b58e96625c6379d8
fweibel <fabien.weibel@gmail.com>
2015-03-14 14:55:22 +0100

    up

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var ejs = require('ejs');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -27,23 +26,20 @@ app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
+/* app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
-});
+}); */
 
 // error handlers
 
 // development error handler
-// will print stacktrace
+/* // will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.render('/error.html', {
-            message: err.message,
-            error: err
-        });
+        res.render( '/error.html' );
     });
 }
 
@@ -51,11 +47,8 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('/error.html', {
-        message: err.message,
-        error: {}
-    });
-});
+    res.render( '/error.html' );
+}); */
 
 module.exports = app;
 
